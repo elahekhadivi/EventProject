17b5a331adcc2ab213b92cb0d0645066a58258e5
elahe_khavidi <elahe.khavidi@akerna.com>
2022-04-25 19:14:16 -0400

    first commit

---
+++ EventProject/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		
+		var number = new Number();
+		number.number = 15;
+
+		var noftify = new EvenNumber();
+		noftify.EvenNumberHappend += number.HandleNotify;
+
+		if(number.number%2 == 0)
+        {
+			noftify.Notify(number.number);
+		}
+		
+	}
+}
+
+
+public class Number
+{
+	public int number { get; set; }
+
+	public void HandleNotify(object sender, EvenNumberEventArgs e)
+	{
+		Console.WriteLine($"{e.EvenNumber} is an even number");
+	}
+
+}
+
+
+public class EvenNumber
+{
+	public event EvenNumberEventArgsHandeler EvenNumberHappend;
+
+	public void Notify(int number)
+	{
+		Console.WriteLine("Alarm went off!");
+		EvenNumberEventArgsHandeler notify = EvenNumberHappend;
+		if (EvenNumberHappend != null)
+		{
+			notify(this, new EvenNumberEventArgs(number));
+		}
+
+	}
+}
+
+
+public delegate void EvenNumberEventArgsHandeler(object source, EvenNumberEventArgs e);
+
+
+
+public class EvenNumberEventArgs : EventArgs
+{
+	public int EvenNumber { get; set; }
+	public EvenNumberEventArgs(int evenNumber)
+	{
+		EvenNumber = evenNumber;
+
+	}
+}
